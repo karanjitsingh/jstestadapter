0367d040a3e1f14ae0eb8c4ef77046b9254b6dcf
Karan Jit Singh <karsin@microsoft.com>
2018-09-20 16:53:45 +0530

    update publish script

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,3 +1,16 @@
 
 . .\base.ps1
-./build.ps1
+
+./build.ps1 -configuration Release -clean
+
+$file = Get-ChildItem ..\artifacts\Release\jstestadapter*.tgz | select -last 1
+
+$filename = $file.Name
+
+write-host $filename
+
+$confirmation = Read-Host "Publish" $filename"? [Y/N]"
+if ($confirmation -eq 'y') {
+    npm login    
+    npm publish $file.fullname
+}
