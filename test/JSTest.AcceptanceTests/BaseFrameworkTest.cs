ef7c0b5709366c392d8a2c813b268c044ac442c6
Karan Jit Singh <karsin@microsoft.com>
2018-12-19 18:01:49 +0530

    update acceptance tests

--- test/JSTest.AcceptanceTests/BaseFrameworkTest.cs
+++ test/JSTest.AcceptanceTests/BaseFrameworkTest.cs
@@ -32,7 +32,10 @@ namespace JSTest.AcceptanceTests
 
         private readonly IEnumerable<string> PreDefinedOutput = new List<string>()
         {
-            "Logging JSTet.Runner Diagnostics in file"
+            "Logging JSTet.Runner Diagnostics in file",
+            "Connected to client with",
+            "Process Launched with id",
+            "Process with id"
         };
 
         #endregion
