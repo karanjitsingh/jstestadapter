80535f08039271f0e4e4dd487f2c9f3c04f93ca6
Karan Jit Singh <karsin@microsoft.com>
2019-01-22 23:49:43 +0530

    refactor

--- src/JSTest/RuntimeProviders/NodeRuntimeProvider.cs
+++ src/JSTest/RuntimeProviders/NodeRuntimeProvider.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace JSTest.RuntimeProviders
+﻿namespace JSTest.RuntimeProviders
 {
     using System;
     using System.Collections.Generic;
