003b498cbf738491a2a699f6146324ee344965ab
Karan Jit Singh <karsin@microsoft.com>
2019-02-17 14:03:47 +0530

    refactor arg processor

---
+++ src/JSTest.Runner/ArgProcessor.ts
@@ -0,0 +1,35 @@
+import { IEnvironment } from './Environment/IEnvironment';
+import { CLIArgs } from './TestRunner/CLIArgs';
+
+ export namespace ArgProcessor {
+
+     export function processCLIArgs(env: IEnvironment): CLIArgs {
+
+        let debugEnabled = false;
+        let debugFilePath = '';
+
+         for (let i = 4; i < env.argv.length; i++) {
+            if (env.argv[i].startsWith('--')) {
+                switch (env.argv[i].substr(2).toLowerCase()) {
+                    case 'diag':
+                        debugEnabled = true;
+                        if (env.argv[++i]) {
+                            debugFilePath = unescape(env.argv[i]);
+                        }
+                        break;
+                    default:
+                        throw new Error('Unknown option ' + env.argv[i]);
+                }
+            } else {
+                throw new Error('Invalid option ' + env.argv[i]);
+            }
+        }
+
+         return <CLIArgs> {
+            ip: env.argv[2],
+            port: Number(env.argv[3]),
+            traceEnabled: debugEnabled,
+            traceFilePath: debugFilePath
+        };
+    }
+} 
