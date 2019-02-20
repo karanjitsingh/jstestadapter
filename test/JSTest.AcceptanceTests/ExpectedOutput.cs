62dd8ffbeed0a22fe9b1b1c4b6b2ea9fe96716d9
Serkan Inci <serkan.inci@hotmail.com>
2019-02-20 02:38:25 -0500

    Enable adding attachments for jest tests. (#32)
    * Enable adding attachments for jest tests.

--- test/JSTest.AcceptanceTests/ExpectedOutput.cs
+++ test/JSTest.AcceptanceTests/ExpectedOutput.cs
@@ -5,15 +5,17 @@ namespace JSTest.AcceptanceTests
 {
     public class ExpectedOutput
     {
-        public ExpectedOutput(List<string> DiscoveryOutput, List<string> ExecutionOutput, List<string> ExecutionWithTestsOutput)
+        public ExpectedOutput(List<string> DiscoveryOutput, List<string> ExecutionOutput, List<string> ExecutionWithTestsOutput, List<string> ExecutionWithAttachmentsOutput)
         {
             this.DiscoveryOutput = DiscoveryOutput;
             this.ExecutionOutput = ExecutionOutput;
             this.ExecutionWithTestsOutput = ExecutionWithTestsOutput;
+            this.ExecutionWithAttachmentsOutput = ExecutionWithAttachmentsOutput;
         }
 
         public List<string> DiscoveryOutput;
         public List<string> ExecutionOutput;
         public List<string> ExecutionWithTestsOutput;
+        public List<string> ExecutionWithAttachmentsOutput;
     }
 }
--- test/JSTest.AcceptanceTests/Jest/__tests__/test.1.js
+++ test/JSTest.AcceptanceTests/Jest/__tests__/test.1.js
@@ -1,9 +1,16 @@
 ﻿const assert = require('assert');
+const fs = require('fs');
+const jstestcontext = require('jstestcontext');
+const path = require('path');
 describe('suite a', () => {
     it('test case a1', () => {
 
     });
     it('test case a2', () => {
         assert.fail('failure');
+    });
+    it('test case a3', function () {
+        const attachmentsDir = jstestcontext.TestContext.Attachments.getTestAttachmentDirectory();
+        fs.writeFileSync(path.join(attachmentsDir, "suite-a.file1.log"), "attachment1 saved");
     });
 })
