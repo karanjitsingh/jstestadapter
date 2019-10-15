f87be9565231240ef82f3c9564bab7c2a9517464
Karan Jit Singh <karan1866@gmail.com>
2019-10-15 14:38:38 +0530

    v1.2.6 (#54)

--- src/JSTest/Constants.cs
+++ src/JSTest/Constants.cs
@@ -9,7 +9,7 @@
         public const string VsTestConnectionTimeout = "VsTestConnectionTimeout";
         public const int MessageProtocolVersion = 1;
         public const int StreamBufferSize = 16384;
-        public const string JSTestVersionInfo = "1.2.5";
+        public const string JSTestVersionInfo = "1.2.6";
 
         public static class TestFrameworkStrings
         {
